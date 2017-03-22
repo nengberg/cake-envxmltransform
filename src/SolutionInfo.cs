08e8a3e96894ebd134239a52650257e3bb1ffec8
nengberg <per.niklas.engberg@gmail.com>
2017-03-22 16:42:25 +0100

    Updates nuspec and project version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Cake.EnvXmlTransform")]
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyFileVersion("0.3.1")]
+[assembly: AssemblyInformationalVersion("0.3.1")]
 [assembly: AssemblyCopyright("Copyright (c) Niklas Engberg 2017")]
 
